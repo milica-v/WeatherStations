e27c9456738bf6c830971cb7a7f68e04da060856
Milica <milica.vukovic@fer.hr>
2024-06-09 11:24:26 +0200

    add empty dashboard component

--- weather-station/src/app/app.component.ts
+++ weather-station/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { WeatherDashboardComponent } from './components/weather-dashboard/weather-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, WeatherDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
