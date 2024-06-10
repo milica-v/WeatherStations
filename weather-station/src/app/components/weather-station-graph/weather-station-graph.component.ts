f42e299c62b0dd1e3b581ae97bbf27adb864da13
Milica Vuković <milica.vukovic@ericsson.com>
2024-06-10 16:32:59 +0200

    add Highcharts chart to WeatherStationComponent

--- weather-station/src/app/components/weather-station-graph/weather-station-graph.component.ts
+++ weather-station/src/app/components/weather-station-graph/weather-station-graph.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from '../../app.service';
+import Highcharts from 'highcharts';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @Component({
   selector: 'app-weather-station-graph',
   standalone: true,
-  imports: [],
+  imports: [HighchartsChartModule],
   templateUrl: './weather-station-graph.component.html',
   styleUrl: './weather-station-graph.component.css',
 })
 export class WeatherStationGraphComponent implements OnInit {
+  Highcharts: typeof Highcharts = Highcharts;
+  data = [1, 2, 3, 4];
+
+  chartOptions: Highcharts.Options = {
+    series: [
+      {
+        type: 'line',
+        data: this.data,
+      },
+    ],
+  };
 
   ngOnInit(): void {
 
